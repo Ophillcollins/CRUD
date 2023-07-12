38519ac19d9f74800a55df01cde5d5804926b4bf
Ophillcollins <andersonsk8.16@hotmail.com>
2023-07-12 11:19:51 -0300

    salvando as alterações

---
+++ crud-spring/src/main/java/com/phillip/Controller/CourseController.java
@@ -0,0 +1,35 @@
+package com.phillip.Controller;
+
+import java.util.List;
+
+import javax.management.loading.ClassLoaderRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.phillip.model.Course;
+import com.phillip.repository.CouseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+
+// endpoint que vai ficar exposto, é no @RequestMapping que conseguimos obter informações do que queremos passar.
+
+@RequestMapping ("/api/courses")
+
+// aqui é criado automaticamente o construtor usando o lombok.
+@AllArgsConstructor
+
+public class CourseController {
+    private final CouseRepository courseRepository;    
+
+    // aqui é onde pega o doGet por tras dos panos para pegar os dados da minha api.
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+    
+}
